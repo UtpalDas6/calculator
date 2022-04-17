da8180bacd56c280b5a4add7cf363e1a59646d26
Utpal <utpaldas.6osm@gmail.com>
2022-04-17 18:45:35 +0530

    Added substraction file

--- calculator.py
+++ calculator.py
@@ -1,6 +1,10 @@
 from addition import add
+from substraction import subtract
 if __name__=="__main__":
     expresion=input("Enter expression:")
     if '+' in expresion:
         e=expresion.split('+')
         print(add(int(e[0]),int(e[1])))
+    if '-' in expresion:
+        e=expresion.split('-')
+        print(subtract(int(e[0]),int(e[1])))
