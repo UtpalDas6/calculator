be21ee7395fec3e711e63cf1cfb9aa3af9c7942a
Utpal <utpaldas.6osm@gmail.com>
2022-04-17 19:20:32 +0530

    added division module

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 from addition import add
 from substraction import subtract
 from multiplication import multiply
+from division import division
 if __name__=="__main__":
     expresion=input("Enter expression:")
     if '+' in expresion:
@@ -12,3 +13,6 @@ if __name__=="__main__":
     if '*' in expresion:
         e=expresion.split('*')
         print(multiply(int(e[0]),int(e[1])))
+    if '/' in expresion:
+        e=expresion.split('/')
+        print(division(int(e[0]),int(e[1])))
