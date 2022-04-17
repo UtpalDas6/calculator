65dd7d759e8efbad8ffe714c0e4d47f15d37c51d
Utpal <utpaldas.6osm@gmail.com>
2022-04-17 19:04:56 +0530

    Added multiplication module

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 from addition import add
 from substraction import subtract
+from multiplication import multiply
 if __name__=="__main__":
     expresion=input("Enter expression:")
     if '+' in expresion:
@@ -8,3 +9,6 @@ if __name__=="__main__":
     if '-' in expresion:
         e=expresion.split('-')
         print(subtract(int(e[0]),int(e[1])))
+    if '*' in expresion:
+        e=expresion.split('*')
+        print(multiply(int(e[0]),int(e[1])))
